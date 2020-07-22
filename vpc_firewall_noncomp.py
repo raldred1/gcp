7be748391cd8ec23c6c68a7241ea2447533242de
raldred1 <ryan@ryanaldred.co.uk>
2020-07-22 17:08:50 +0100

    Ingress handling + exception handling

--- vpc_firewall_noncomp.py
+++ vpc_firewall_noncomp.py
@@ -1,12 +1,16 @@
 from google.cloud import resource_manager
 import google.auth
 from googleapiclient import discovery
+import sys
+from googleapiclient.errors import HttpError
 
-# *** CURRENTLY SCANNING EGRESS ONLY ***
+# TO DO
+# 1. 'Other Protocols' -> such as SSH
+# 2. Action on Match -> Allow
+# 3. Disabled Rule (Enabled only)
 
 credentials, project = google.auth.default()
 client = resource_manager.Client()
-#service = discovery.build('compute', 'v1', credentials=credentials)
 
 f = open("vpc_firewall_noncomp_output.csv", "w")
 find_bad_ports = ['80', '21'] # Provide as strings (not var)
@@ -31,14 +35,14 @@ def main():
     service = discovery.build('compute', 'v1', credentials=credentials)
     for project in client.list_projects():
         project = project.project_id
-        #found_port = False
         # Temp: Just check the RA project (seceng-ra-001) whilst testing...
-        if project.upper() == 'seceng-ra-001'.upper():
-            try:
-                #request = service.firewalls().list(project=project, filter='direction=EGRESS')
-                request = service.firewalls().list(project=project)
-                response = request.execute()
-                
+        #if project.upper() == 'chencompliance'.upper(): <--- USE TO TEST JUST ONE PROJECT
+        try:
+            #request = service.firewalls().list(project=project, filter='direction=EGRESS')
+            request = service.firewalls().list(project=project)
+            response = request.execute()
+            
+            if response.get('items','no_items') != 'no_items': # Ensure 'items' exists
                 for firewall in response['items']:
                     if firewall["direction"] == 'EGRESS':
                         firewall_ip_range = firewall["destinationRanges"]
@@ -54,50 +58,57 @@ def main():
                                     output(f'{project},{firewall["name"]},public target: {ip_range}\n','a')
                             elif ip_range.startswith('172.') is False:
                                 output(f'{project},{firewall["name"]},public target: {ip_range}\n','a')
-                
-                    for allow in firewall["allowed"]:
-                        current_ipprotocol = allow['IPProtocol']
 
-                        try:
-                            if allow['ports'] != None:
-                                ports_listed = allow['ports']
-                        except:
-                            ports_listed = None
+                    if firewall.get('allowed','must_be_deny') != 'must_be_deny': # If can't find 'allowed' then must be a deny.
+                        for allow in firewall["allowed"]:
+                            current_ipprotocol = allow['IPProtocol']
 
-                        # Look for IPProtocol = 'all' or IPProtocol = 'tcp/udp' (but no ports specified, i.e. 'all')
-                        if current_ipprotocol == 'all':
-                            output(f'{project},{firewall["name"]},tcp/udp:all ports\n','a')
-                        elif current_ipprotocol == 'tcp' and ports_listed is None:
-                            output(f'{project},{firewall["name"]},tcp:all ports\n','a')
-                        elif current_ipprotocol == 'udp' and ports_listed is None:
-                            output(f'{project},{firewall["name"]},udp:all ports\n','a')
+                            try:
+                                if allow['ports'] != None:
+                                    ports_listed = allow['ports']
+                            except:
+                                ports_listed = None
 
-                        # Look for IPProtocol = 'tcp' AND ports = (find_bad_ports list)
-                        try: 
-                            bad_ports_found = [] 
-                            for port in allow['ports']:
+                            # Look for IPProtocol = 'all' or IPProtocol = 'tcp/udp' (but no ports specified, i.e. 'all')
+                            if current_ipprotocol == 'all':
+                                output(f'{project},{firewall["name"]},tcp/udp:all ports\n','a')
+                            elif current_ipprotocol == 'tcp' and ports_listed is None:
+                                output(f'{project},{firewall["name"]},tcp:all ports\n','a')
+                            elif current_ipprotocol == 'udp' and ports_listed is None:
+                                output(f'{project},{firewall["name"]},udp:all ports\n','a')
 
-                                for port_check in find_bad_ports:
-                                    if current_ipprotocol == 'tcp' and port == port_check:
-                                        # Found a bad port...
-                                        bad_ports_found.append(port)
+                            # Look for IPProtocol = 'tcp' AND ports = (find_bad_ports list)
+                            try: 
+                                bad_ports_found = [] 
+                                for port in allow['ports']:
 
-                                    if port.find('-') != -1:
-                                        port_range = port.split('-')
-                                        if current_ipprotocol == 'tcp' and port_range is not None and int(port_range[0]) < int(port_check) and int(port_range[1]) > int(port_check):
-                                            # Found a bad port (in range)...
+                                    for port_check in find_bad_ports:
+                                        if current_ipprotocol == 'tcp' and port == port_check:
+                                            # Found a bad port...
                                             bad_ports_found.append(port)
-                                        
-                            if len(bad_ports_found) != 0:
-                                output(f'{project},{firewall["name"]},bad port found: {bad_ports_found}\n','a')
 
-                        except:
-                            pass
+                                        if port.find('-') != -1:
+                                            port_range = port.split('-')
+                                            if current_ipprotocol == 'tcp' and port_range is not None and int(port_range[0]) < int(port_check) and int(port_range[1]) > int(port_check):
+                                                # Found a bad port (in range)...
+                                                bad_ports_found.append(port)
+                                            
+                                if len(bad_ports_found) != 0:
+                                    output(f'{project},{firewall["name"]},bad port found: {bad_ports_found}\n','a')
+
+                            except:
+                                pass
                     
-            except:
-                output('*Exception*','a')
-            
-    
+        except HttpError as e:
+            if e.resp.status == 403:
+                #output(f'Project [{project}] Compute API not enabled','a')
+                pass # Do nothing as this means there aren't any non-compliance FW rules
+            #else:
+                #print(e.resp.status)
+        except:
+            output(f"Project: {project} | Unexpected error: {sys.exc_info()[0]}","a")
+
+
 
 if __name__ == "__main__":
     welcome()
