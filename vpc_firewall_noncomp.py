a774726c399c40fba8e5bd15af599d456e253772
raldred1 <ryan@ryanaldred.co.uk>
2020-07-22 11:25:48 +0100

    + ingress support + 172 & 192.168 support

--- vpc_firewall_noncomp.py
+++ vpc_firewall_noncomp.py
@@ -9,7 +9,7 @@ client = resource_manager.Client()
 #service = discovery.build('compute', 'v1', credentials=credentials)
 
 f = open("vpc_firewall_noncomp_output.csv", "w")
-find_bad_ports = ['80']
+find_bad_ports = ['80', '21'] # Provide as strings (not var)
 
 def output(output_msg,output_type):
     # type: p/f/a (print/file/all) [blank = all]
@@ -35,16 +35,25 @@ def main():
         # Temp: Just check the RA project (seceng-ra-001) whilst testing...
         if project.upper() == 'seceng-ra-001'.upper():
             try:
-                request = service.firewalls().list(project=project, filter='direction=EGRESS')
+                #request = service.firewalls().list(project=project, filter='direction=EGRESS')
+                request = service.firewalls().list(project=project)
                 response = request.execute()
                 
                 for firewall in response['items']:
-                    for dest_range in firewall["destinationRanges"]:
-                        if dest_range.find('0.0.0.0') != -1:
-                            output(f'{project},{firewall["name"]},destination: 0.0.0.0\n','a')
-                        if dest_range.startswith('10.') is False:
-                            output(f'{project},{firewall["name"]},public target: {dest_range}\n','a')
+                    if firewall["direction"] == 'EGRESS':
+                        firewall_ip_range = firewall["destinationRanges"]
+                    elif firewall["direction"] == 'INGRESS':
+                        firewall_ip_range = firewall["sourceRanges"]
 
+                    for ip_range in firewall_ip_range:
+                        ip_ip_split = ip_range.split('.') # Split for 172.x.x.x query
+                        if ip_range.find('0.0.0.0') != -1:
+                            output(f'{project},{firewall["name"]},ip_range: 0.0.0.0\n','a')
+                        elif ip_range.startswith('10.') is False and ip_range.startswith('192.168.') is False:
+                            if ip_range.startswith('172.') and (int(ip_ip_split[1]) < 16 or int(ip_ip_split[1]) > 31): # i.e. not between 172.16.0.0 – 172.31.255.255
+                                    output(f'{project},{firewall["name"]},public target: {ip_range}\n','a')
+                            elif ip_range.startswith('172.') is False:
+                                output(f'{project},{firewall["name"]},public target: {ip_range}\n','a')
                 
                     for allow in firewall["allowed"]:
                         current_ipprotocol = allow['IPProtocol']
@@ -80,13 +89,13 @@ def main():
                                             bad_ports_found.append(port)
                                         
                             if len(bad_ports_found) != 0:
-                                output(f'{project},{firewall["name"]},Bad port found\n','a')
+                                output(f'{project},{firewall["name"]},bad port found: {bad_ports_found}\n','a')
 
                         except:
                             pass
                     
             except:
-                pass
+                output('*Exception*','a')
             
     
 
